c8bda25e9bebd52ec26d9ff835c62daab1170f49
ekolpakova <konanhayumi.kh@gmail.com>
2022-06-07 16:35:52 +0300

    Tests

--- src/main/kotlin/com/spring/inventory/controllers/TestController.kt
+++ src/main/kotlin/com/spring/inventory/controllers/TestController.kt
@@ -5,9 +5,11 @@ import com.spring.inventory.repositories.TestRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("api/v1/public")
 class TestController {
     @Autowired
     lateinit var testRepo: TestRepository;
