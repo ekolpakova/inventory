69ee20d85cb1d7dbec699eb0d4987c85bf712847
ekolpakova <konanhayumi.kh@gmail.com>
2022-06-07 11:24:18 +0300

    Netlify config

--- src/main/kotlin/com/spring/inventory/config/CorsConfig.kt
+++ src/main/kotlin/com/spring/inventory/config/CorsConfig.kt
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletResponse
 class CorsConfig: Filter {
     override fun doFilter(req: ServletRequest?, res: ServletResponse?, chain: FilterChain?) {
         val response = res as (HttpServletResponse)
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000")
         response.setHeader("Access-Control-Allow-Origin", "https://inventory-react.netlify.app")
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, UPDATE")
         response.setHeader("Access-Control-Max-Age", "3600")
