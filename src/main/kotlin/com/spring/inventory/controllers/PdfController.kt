720855201bf221fbe532fe7cab08b6ccfcb14e04
ekolpakova <konanhayumi.kh@gmail.com>
2022-06-13 01:54:23 +0300

    Pdf generation

--- src/main/kotlin/com/spring/inventory/controllers/PdfController.kt
+++ src/main/kotlin/com/spring/inventory/controllers/PdfController.kt
@@ -1,10 +1,6 @@
 package com.spring.inventory.controllers
 
-import com.itextpdf.text.BaseColor
-import com.itextpdf.text.Document
-import com.itextpdf.text.Font
-import com.itextpdf.text.Paragraph
-import com.itextpdf.text.Phrase
+import com.itextpdf.text.*
 import com.itextpdf.text.pdf.BaseFont
 import com.itextpdf.text.pdf.PdfPCell
 import com.itextpdf.text.pdf.PdfPTable
@@ -12,12 +8,18 @@ import com.itextpdf.text.pdf.PdfWriter
 import com.spring.inventory.entities.Fix
 import com.spring.inventory.services.FixService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.io.ByteArrayOutputStream
 import java.io.FileOutputStream
 import java.util.stream.Stream
+
 
 @RestController
 @Transactional
@@ -28,15 +30,20 @@ class PdfController {
     lateinit var fixService: FixService
 
     @GetMapping("createTable")
-    fun createTable(): Document {
+    fun createTable() {
         val document = Document()
 
         val font_ = "/home/lisa/IdeaProjects/inventory/src/main/kotlin/com/spring/inventory/fonts/timesnewroman.ttf"
         val bf = BaseFont.createFont(font_, BaseFont.IDENTITY_H, BaseFont.EMBEDDED)
         val font = Font(bf, 12.0f, Font.NORMAL)
 
+        val byteArrayOutputStream = ByteArrayOutputStream()
+
         //val font:Font = Font(Font.FontFamily.TIMES_ROMAN, 14.0f)
-        PdfWriter.getInstance(document, FileOutputStream("Data.pdf"))
+        //PdfWriter.getInstance(document, FileOutputStream("Data.pdf"))
+        PdfWriter.getInstance(document, byteArrayOutputStream)
+
+
 
         document.open()
 
@@ -54,7 +61,27 @@ class PdfController {
         document.add(table)
         document.close()
 
-        return document
+        val bytes = byteArrayOutputStream.toByteArray()
+
+        val headers = HttpHeaders()
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=Data.pdf");
+
+        val home = System.getProperty("user.home")
+        val fos = FileOutputStream("$home/Downloads/Data.pdf")
+        fos.write(bytes)
+        //return ResponseEntity.ok().headers(headers).contentLength(bytes.size.toLong()).contentType(MediaType.APPLICATION_OCTET_STREAM).body(ByteArrayResource(bytes))
+        //return document
+    }
+    fun clientOutput() {
+        val contents = createTable()
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_PDF
+        // Here you have to set the actual filename of your pdf
+        // Here you have to set the actual filename of your pdf
+        val filename = "output.pdf"
+        headers.setContentDispositionFormData(filename, filename)
+        headers.cacheControl = "must-revalidate, post-check=0, pre-check=0"
+        val response: Any? = ResponseEntity<Any?>(contents, headers, HttpStatus.OK)
     }
     fun addTableHeader(table: PdfPTable, font: Font) {
 
